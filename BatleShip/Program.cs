7da45d791b38becdfb40e0baa278dcdc2b0edc55
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-08 11:47:59 +0400

    Back

--- BatleShip/Program.cs
+++ BatleShip/Program.cs
@@ -11,19 +11,26 @@ namespace BatleShip
 
         static void Main(string[] args)
         {
-            TransportShip ship = new TransportShip();
-            string res = ship.Move(3000);
+            BaseShip ship = GetShip(ShipType.FightShip);
+            string result = ship.Fight();
             Console.WriteLine(res);
             Console.ReadLine();
+        }
+        static BaseShip GetShip(ShipType shipType)
+        {
+            switch (shipType)
+            {
+                case ShipType.FightShip:
+                    return new FightShip();
+                case ShipType.TransportShip:
+                    return new TransportShip();
+                default:
+                    throw new Exception("Неизвестный корабль");
+            }
         }
     }
     public class BaseShip
     {
-        public BaseShip()
-        {
-            //Console.WriteLine("Вызван конструктер родительского класса");
-        }
-
         private int _counter;
 
         protected int _speed;
@@ -34,29 +41,50 @@ namespace BatleShip
             string result = string.Format("Пройдено километров: {0}", distance);
             return result;
         }
+        public virtual string Fight()
+        {
+            return "Произошол бой";
+        }
+        public enum ShipType
+        {
+            TranspotrShip = 1,
+            FightShip = 2,
+        }
     }
+    public class FightShip : BaseShip
+    {
+        public override string Move(int distance)
+        {
+            double time = (double)distance / 2000;
+            return string.Format("Пройдено километров:{0} за время {1}", distance, time);
+        }
+        public override string Fight()
+        {
+            return "Противник уничтожен. ";
+        }
 
-    public class AnotherBaseShip
-    { 
-
-    }
+        public override string ToString()
+        {
+            return "Боевой корабль";
+        }
+     }
     public class TransportShip : BaseShip
     {
         public override string Move(int distance)
         {
-            string result = base.Move(distance);
-            result += string.Format("Транспортный корабаль преодалел километров: {0}", distance);
-            return result;
+            double time = (double)distance / 2000;
+            return string.Format("Пройдено километров:{0} за время {1}", distance, time);
         }
-        public TransportShip()
+        public override string Fight()
         {
-            //Console.WriteLine("Вызван коструктер дочернего класса");
+            return "Транспотрный корабль не может вступить в бой!";
         }
 
-       public void Start()
-       {
-
-       }
-     }
+        public override string ToString()
+        {
+            return "Транспотрный корабль";
+        }
     
+    }
+
 }
